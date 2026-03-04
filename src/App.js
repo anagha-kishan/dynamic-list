e995b5f8d67c81e648a7479d19b4358321527983
anagha-kishan <anaghas686@gmail.com>
2026-03-04 11:20:11 +0530

    final

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import './style.css';
 export default function App() {
   const [cities, setCities] = useState(['Chennai', 'Delhi', 'Kochi']);
   const [value, setValue] = useState('');
-
+  const [error, setError] = useState('');
   const handleSubmit = () => {
+    if (value === '') {
+      setError('*Enter value');
+      return;
+    }
     setCities((prev) => [...prev, value]);
-
+    setError('');
     setValue('');
   };
   return (
@@ -18,6 +22,7 @@ export default function App() {
         onChange={(e) => setValue(e.target.value)}
       />
       <button onClick={handleSubmit}>Submit</button>
+      <p>{error}</p>
       <ol>
         {cities.map((item, index) => (
           <li>{item}</li>
